954204e02068ea6fc35749576695639aef524cd6
Gabriel Schorsch Amadeu <49549303837@info.local>
2019-08-19 17:22:35 -0300

    Primeiro commit + Exercicio Senai.Filmes.WebApi

---
+++ Senai.Filmes.WebApi/Senai.Filmes.WebApi/Controllers/GenerosController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Filmes.WebApi.Domains;
+using Senai.Filmes.WebApi.Repositories;
+
+namespace Senai.Filmes.WebApi.Controllers {
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+
+
+    public class GenerosController : ControllerBase {
+
+        GenerosRepository generoRepository = new GenerosRepository();
+
+
+        [HttpGet]
+        public IEnumerable<GeneroDomain> Get() {
+            
+            return generoRepository.Listar();
+            }
+    }
+}
---
+++ Senai.Filmes.WebApi/Senai.Filmes.WebApi/Repositories/GenerosRepository.cs
@@ -0,0 +1,36 @@
+﻿using Senai.Filmes.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Filmes.WebApi.Repositories {
+    public class GenerosRepository {
+        private string StringConexao = "Data Source=.\\SqlExpress;Initial Catalog=RoteiroFilmes;User Id=sa;Pwd=132;";
+
+        public List<GeneroDomain> Listar() {
+            List<GeneroDomain> lista = new List<GeneroDomain>();
+
+            SqlConnection con = new SqlConnection(StringConexao);
+            string Query = "SELECT * FROM Generos";
+            con.Open();
+
+            SqlDataReader sdr;
+
+            SqlCommand cmd = new SqlCommand(Query, con);
+
+            sdr = cmd.ExecuteReader();
+            while (sdr.Read()) {
+                GeneroDomain genero = new GeneroDomain {
+                    IdGenero = Convert.ToInt32(sdr["IdGenero"]),
+                    Nome = sdr["Nome"].ToString()
+                };
+                lista.Add(genero);
+            }
+            return lista;
+        }
+
+
+    }
+}
